e9dca9235d37c44b29ed2ba0212f770166b2701c
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-14 09:09:26 +0100

    Create database in file when it doesn't exist

---
+++ create-database.ts
@@ -0,0 +1,30 @@
+import postgres from "postgres";
+import env from "./src/env";
+
+const DATABASE_NAME = env.POSTGRES_DATABASE;
+
+const psql = postgres("", {
+  host: env.POSTGRES_HOST,
+  port: env.POSTGRES_PORT,
+  user: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD,
+});
+
+try {
+  const databaseExists = await psql`
+      SELECT 1 FROM pg_database WHERE datname = ${DATABASE_NAME}
+    `;
+
+  if (!databaseExists.length) {
+    await psql`CREATE DATABASE ${DATABASE_NAME}`;
+
+    console.log(`Database ${DATABASE_NAME} created`);
+  } else {
+    console.log(`Database ${DATABASE_NAME} already exists`);
+  }
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+process.exit(0);
